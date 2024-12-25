fa45dc4e1888401c6def6694db7775fa3da61a27
vnszero <vnszero@gmail.com>
2024-12-25 22:35:30 +0000

    start send and receive

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # rabbitmq-model
 this is a start rabbitmq model in python
+
+## Installation and Run Command:
+docker run -it --rm --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:4.0-management
---
+++ receive.py
@@ -0,0 +1,28 @@
+import pika, sys, os
+
+def main():
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+    channel = connection.channel()
+
+    channel.queue_declare(queue='hello')
+
+    def callback(ch, method, properties, body):
+        print(f" [x] Received {ch}")
+        print(f" [x] Received {method}")
+        print(f" [x] Received {properties}")
+        print(f" [x] Received {body}")
+
+    channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
+
+    print(' [*] Wainting for messages. To exit press CTRL+C')
+    channel.start_consuming()
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Interrupted')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
