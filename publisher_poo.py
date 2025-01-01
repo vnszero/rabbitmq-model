112d4bd009df7756ef82cd727c8a454d5e7a7702
vnszero <vnszero@gmail.com>
2025-01-01 17:57:30 +0000

    update publisher

--- publisher_poo.py
+++ publisher_poo.py
@@ -8,8 +8,7 @@ class RabbitmqPublisher:
         self.__port = 5672
         self.__username = "guest"
         self.__password = "guest"
-        self.__exchange = "data_exchange"
-        self.__routing_key = "" # this is useful when a exchange has to fill different queues
+        self.__exchange = "microservice_exchange"
         self.__channel = self.__create_channel()
     
     def __create_channel(self):
@@ -21,19 +20,21 @@ class RabbitmqPublisher:
                 password=self.__password
             )
         )
-
-        channel = pika.BlockingConnection(connection_parameters).channel()
+        connection = pika.BlockingConnection(connection_parameters)
+        channel = connection.channel()
+        channel.exchange_declare(exchange=self.__exchange, exchange_type='direct', durable=True)
         return channel
     
-    def send_message(self, body: Dict):
+    def send_message(self, routing_key: str, body: Dict):
         self.__channel.basic_publish(
             exchange=self.__exchange,
-            routing_key=self.__routing_key,
+            routing_key=routing_key,
             body=json.dumps(body),
             properties=pika.BasicProperties(
-                delivery_mode=2
+                delivery_mode=2 # Persistent messages
             )
         )
 
 rabbitmq_publisher = RabbitmqPublisher()
-rabbitmq_publisher.send_message({"Ola": "Mundo"})
+# the routing key is useful when a exchange has to fill different queues
+rabbitmq_publisher.send_message("", {"Ola": "Mundo"})
